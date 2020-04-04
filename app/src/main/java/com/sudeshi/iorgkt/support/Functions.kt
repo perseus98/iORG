84dcc2fd45238b0e4824952dbe97e3af14764a6f
perseus98 <perseusmhrn@gmail.com>
2020-04-04 21:13:33 +0530

    baseline-model-done-unchecked

--- app/src/main/java/com/sudeshi/iorgkt/support/Functions.kt
+++ app/src/main/java/com/sudeshi/iorgkt/support/Functions.kt
@@ -1,3 +1,34 @@
 package com.sudeshi.iorgkt.support
 
-object Functions
+import android.app.DatePickerDialog
+import java.text.SimpleDateFormat
+import java.util.*
+
+object Functions {
+    private fun initChipGroup() {
+//        chip.isClickable = true
+//        chip.isCheckable = false
+    }
+
+    private fun initCalanderDialog() {
+        val cal = Calendar.getInstance()
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                val myFormat = "dd/MM/yyyy, HH:mm:ss" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.US)
+//                outlinedTextCalander?.editTextCalander?.setText(sdf.format(cal.time))
+            }
+
+//        outlinedTextCalander.editTextCalander.setOnClickListener {
+//            DatePickerDialog(
+////                this@CreateActivity, R.style.styledatepicker, dateSetListener,
+//                cal.get(Calendar.YEAR),
+//                cal.get(Calendar.MONTH),
+//                cal.get(Calendar.DAY_OF_MONTH)
+//            ).show()
+    }
+}
